f80ba4e0fd18625e98a0df0597b8db7c8a909994
Chej Sidi <csidi@egamaster.es>
2023-06-22 15:45:31 +0200

    Agregamos variables de entorno

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const http = require("http")
 
 function requestControler () {
@@ -7,5 +9,7 @@ function requestControler () {
 
 // Configuración del srv
 const server = http.createServer(requestControler)
-
-server.listen(4000)
+const PORT = process.env.PORT 
+server.listen(PORT, function () {
+    console.log("Aplicación corriendo en el puerto: " + PORT )
+})
